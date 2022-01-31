b7c6710f88b0b749d9744420db14fd375d2e2340
eyeofhorus <eshaanmandal>
2022-01-31 16:16:20 +0530

    Added the additional preprocess method

--- assignment1.py
+++ assignment1.py
@@ -3,7 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def split_n_shuffle(image):
-    '''Divides the image into 4 parts'''
+    '''
+    Divides the image into 4 parts
+    and arranges it according to the question
+    '''
     row, col = image.shape
     p1, p2, p3, p4 = image[0:int(row/2), 0:int(col/2)], image[0:int(row/2),int(col/2):col+1], image[int(row/2):row+1, 0:int(col/2)], image[int(row/2):row+1, int(col/2):col+1]
     print(p1.shape, p2.shape, p3.shape, p4.shape)
@@ -14,8 +17,22 @@ def split_n_shuffle(image):
     shuffled_image[int(row/2):row+1, int(col/2):col+1] = p3
 
     return shuffled_image
-img_path = './pikachu.png'
-image = cv.cvtColor(cv.imread(img_path),cv.COLOR_BGR2GRAY)
+def preprocess(image):
+    '''Applies the required preprocessing for the image
+    -> converts RGB to Grayscale
+    -> Converts Odd numbers of rows and columns to even number of rows of columns
+    '''
+    if len(image.shape) == 3:
+        image = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+    if image.shape[0] % 2 != 0:
+        image = np.delete(image, -1, axis=0)
+    if image.shape[1] % 2 != 0:
+        image = np.delete(image, -1, axis=1)
+    return image
+
+
+img_path = './bird.jpeg'
+image = preprocess(cv.imread(img_path))
 shuffled_image = split_n_shuffle(image)
 
 images = [image, shuffled_image]
