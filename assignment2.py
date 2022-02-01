62f12607c46c39e653c24041e306cd6393b9e63e
eyeofhorus <eshaanmandal>
2022-02-01 13:38:37 +0530

    assignment 2

---
+++ assignment2.py
@@ -0,0 +1,36 @@
+from turtle import color
+import cv2 as cv
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+plt.rcParams['figure.figsize']=[6,6]
+
+
+# numberofbins = ceil( (maximumvalue - minimumvalue) / binwidth 
+def preprocess(image):
+    '''Applies the required preprocessing for the image
+    -> converts RGB to Grayscale
+    -> reduce the image resolution to make computations little bit easy for the computer 
+    '''
+    if len(image.shape) == 3:
+        image = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+    if image.shape[0]*image.shape[1] > 10_000:
+        image = cv.resize(image, (100,100))
+    return image
+
+def histogram(img, bins=256):
+    '''Calculates the histogram for the image, bins can be provided by the user or it assumes bins = 256 (8-bit image)'''
+    hist_array = [0 for _ in range(bins)]
+    for i in img:
+        for j in i:
+            hist_array[math.ceil(int(j)*(bins-1)/255)]+=1
+    return  hist_array
+    
+
+img = cv.imread('sample.jpg')
+img = preprocess(img)
+nbins = 64
+h = histogram(img, bins=nbins)
+plt.bar(range(nbins),h,color='green')
+plt.show()
+
