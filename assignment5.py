f1d9320a267808e312003479c39b7348571450b0
eshaanmandal <eshaanmandal29@gmailcom>
2022-03-31 21:13:38 +0530

    Convolution operation on images

---
+++ assignment5.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+def convolve(X, F):
+    # height and width of the image
+    X_height = X.shape[0]
+    X_width = X.shape[1]
+    
+    # height and width of the filter
+    F_height = F.shape[0]
+    F_width = F.shape[1]
+    
+    H = (F_height - 1) // 2
+    W = (F_width - 1) // 2
+    
+    #output numpy matrix with height and width
+    out = np.zeros((X_height, X_width))
+    #iterate over all the pixel of image X
+    for i in np.arange(H, X_height-H):
+        for j in np.arange(W, X_width-W):
+            sum = 0
+            #iterate over the filter
+            for k in np.arange(-H, H+1):
+                for l in np.arange(-W, W+1):
+                    #get the corresponding value from image and filter
+                    a = X[i+k, j+l]
+                    w = F[H+k, W+l]
+                    sum += (w * a)
+            out[i,j] = sum
+    #return convolution  
+    return out
+
+
