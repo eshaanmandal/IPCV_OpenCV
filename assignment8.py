d1d20d81601c3f30f93e10cbbc59fc5f869f79e9
eshaanmandal <eshaanmandal29@gmail.com>
2022-05-16 10:23:42 +0530

    Modified rotate function which doesnt crop images

--- assignment8.py
+++ assignment8.py
@@ -2,15 +2,45 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
+def rotateImage(rotateImage, angle):
+	
+	# Taking image height and width
+	imgHeight, imgWidth = rotateImage.shape[0], rotateImage.shape[1]
 
-def rotate_image(image, angle):
-  image_center = tuple(np.array(image.shape[1::-1]) / 2)
-  rot_mat = cv.getRotationMatrix2D(image_center, angle, 1.0)
-  result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR)
-  return result
+	# Computing the centre x,y coordinates
+	# of an image
+	centreY, centreX = imgHeight//2, imgWidth//2
+
+	# Computing 2D rotation Matrix to rotate an image
+	rotationMatrix = cv.getRotationMatrix2D((centreY, centreX), angle, 1.0)
+
+	# Now will take out sin and cos values from rotationMatrix
+	# Also used numpy absolute function to make positive value
+	cosofRotationMatrix = np.abs(rotationMatrix[0][0])
+	sinofRotationMatrix = np.abs(rotationMatrix[0][1])
+
+	# Now will compute new height & width of
+	# an image so that we can use it in
+	# warpAffine function to prevent cropping of image sides
+	newImageHeight = int((imgHeight * sinofRotationMatrix) +
+						(imgWidth * cosofRotationMatrix))
+	newImageWidth = int((imgHeight * cosofRotationMatrix) +
+						(imgWidth * sinofRotationMatrix))
+
+	# After computing the new height & width of an image
+	# we also need to update the values of rotation matrix
+	rotationMatrix[0][2] += (newImageWidth/2) - centreX
+	rotationMatrix[1][2] += (newImageHeight/2) - centreY
+
+	# Now, we will perform actual image rotation
+	rotatingimage = cv.warpAffine(
+		rotateImage, rotationMatrix, (newImageWidth, newImageHeight))
+
+	return rotatingimage
+
 
 img = cv.imread('cuboid.jpeg')
-rotated_img = rotate_image(img, 45)
+rotated_img = rotateImage(img, 120)
 
 plt.imshow(rotated_img)
 plt.show()
